c43c3c91dcd438036d7bb27f985231004a017a24
Marin @Home <marin@marincarroll.com>
2025-09-28 13:57:38 -0400

    namespacing

--- video-with-text-track.php
+++ video-with-text-track.php
@@ -2,7 +2,7 @@
 namespace Marincarroll\VideoWithTextTrack;
 
 /**
- * Plugin Name:       Video with Text Trck
+ * Plugin Name:       Video with Text Track
  * Description:       Displays a muted video with a synced text track beside it.
  * Version:           0.1.0
  * Requires at least: 6.7
@@ -10,15 +10,19 @@ namespace Marincarroll\VideoWithTextTrack;
  * Author:            Marin Carroll
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       video-with-text-track
+ * Text Domain:       marincarroll
  *
  * @package Marincarroll
+ * @subpackage VideoWithTextTrack
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * TODO
+ */
 function register_video_text_track_block_type() {
 	register_block_type( __DIR__ . "/build/block.json" );
 }
